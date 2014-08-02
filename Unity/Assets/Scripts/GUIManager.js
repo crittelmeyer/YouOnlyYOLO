1d7ae6759d822043346dc688127f798e2db49ea1
Chris Rittelmeyer <github.10.iamchris@spamgourmet.com>
2014-08-02 14:26:10 -0400

    Created GUIManager and basic health bar code

---
+++ Unity/Assets/Scripts/GUIManager.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+private var healthBarLength : float;
+private var currentHealth : int = 100;
+private var maxHealth : int = 100;
+
+function Start() {
+	healthBarLength = Screen.width / 2;
+}
+
+function Update() {
+	AdjustCurrentHealth(0);
+}
+
+function OnGUI() {
+	GUI.Box(new Rect(10, 10, healthBarLength, 20), currentHealth.ToString() + "/" + maxHealth.ToString());
+}
+
+function AdjustCurrentHealth (adj : int) {
+   currentHealth += adj;
+   
+   if (currentHealth < 0) currentHealth = 0;
+   
+   if (currentHealth > maxHealth) currentHealth = maxHealth;
+   
+   if (maxHealth < 1) maxHealth = 1;
+   
+   healthBarLength = (Screen.width / 2) * (currentHealth / maxHealth);
+}
